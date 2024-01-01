82173f080ed02f1e64627fb5f3cd9482e9100546
Felix Pultar <felix.pultar@phys.chem.ethz.ch>
2024-01-02 00:40:26 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,5 +5,7 @@ struct Vector3 {
 };
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+  vec1 = Vector3(0.0f, 1.0f, -3.2f);
+  vec2 = Vector3(-1.1f, 20.0f, 32.1f);
+  std::cout << vec1.x + vec2.x << std::endl;
 }
